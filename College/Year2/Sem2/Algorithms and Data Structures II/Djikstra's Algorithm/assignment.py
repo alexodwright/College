ae5c5a5a60ee397a18dbe841014bb242a8e05ee5
Alex <alexodwright@gmail.com>
2025-03-10 19:09:32 +0000

    Altered output to be in correct order

--- College/Year2/Sem2/Algorithms and Data Structures II/Djikstra's Algorithm/assignment.py
+++ College/Year2/Sem2/Algorithms and Data Structures II/Djikstra's Algorithm/assignment.py
@@ -7,49 +7,38 @@ from apq import APQUL, APQBH
 def part_1() -> None:
     graph = graphreader("simplegraph2-2.txt")
     s = graph.get_vertex_by_label(14)
-    res = djikstra(s, graph, APQUL)
+    res = djikstra(s, graph, APQBH)
     path(graph.get_vertex_by_label(5), res)
-    # print("Unordered List: ")
-    # for k, v in res.items():
-        # print(f"Target: {k.__str__()}:, Cost: {res[k][0]}, Predecessor: {res[k][1].__str__()}")
-
-    # print("Min Heap: ")
-    # res = djikstra(s, graph, APQBH)
-    # for k, v in res.items():
-        # print(f"target: {k.__str__()}:, cost: {res[k][0]}, predecessor: {res[k][1].__str__()}")
 
 def part_2() -> None:
     n = int(input("Enter n: "))
     m = int(input("Enter m: "))
     graph = create_random_graph(n, m)
     s = graph.get_vertex_by_label(1)
-    res = djikstra(s, graph, APQUL)
-    for k, v in res.items():
-        print(f"Target: {k.__str__()}:, Cost: {res[k][0]}, Predecessor: {res[k][1].__str__()}")
-    # print([f"Target: {res[k].__str__()}:, Cost: {res[k][0]}, Predecessor: {res[k][1].__str__()}" for k in res])
+    res = djikstra(s, graph, APQBH)
 
 def part_3() -> None:
     for i in range(1, 11):
         graph = create_random_graph(i, i)
         s = graph.get_vertex_by_label(1)
-        res = time_func(djikstra, s, graph, APQUL)
-        k = graph.get_vertex_by_label(i*i)
-        print(f"Target: {k.__str__()}:, Cost: {res[k][0]}, Predecessor: {res[k][1].__str__()}")
+        exec_time, res = time_func(djikstra, s, graph, APQBH)
+        print(f"Djikstra took {exec_time:.8f}s to execute with a {i}x{i} random graph.")
+        print(f"Shortest path from node 1 to node {i*i} is ", end="")
+        path(graph.get_vertex_by_label(i*i), res)
 
 def main() -> None:
-    part_1()
+    # part_1()
     # part_2()
-    # part_3()
+    part_3()
 
 def path(end, closed):
-    for k, v in closed.items():
-        print(f"target: {k.__str__()}:, cost: {closed[k][0]}, predecessor: {closed[k][1].__str__()}")
     pred = end
-    output = f"{end.element()}<-"
+    output = [end.element()]
     while pred is not None:
-        output += f"{closed[pred][1].__str__()}<-"
+        if closed[pred][1] is not None:
+            output.append(closed[pred][1].element())
         pred = closed[pred][1]
-    output = output[:-8]
+    output = "->".join(map(str, output[::-1]))
     print(output)
     
 if __name__=="__main__":
--- College/Year2/Sem2/Algorithms and Data Structures II/Djikstra's Algorithm/time_func.py
+++ College/Year2/Sem2/Algorithms and Data Structures II/Djikstra's Algorithm/time_func.py
@@ -4,5 +4,5 @@ def time_func(func, *args, **kwargs):
     start_time = perf_counter()
     res = func(*args, **kwargs)
     end_time = perf_counter()
-    print(f"{func.__name__} took {(end_time - start_time):.8f}s to execute")
-    return res
+    exec_time = end_time - start_time
+    return exec_time, res
